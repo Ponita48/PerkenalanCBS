504bfa64984374c2d59392655b2cb491c5cc3c00
atepagung <agungteja64@gmail.com>
2017-05-24 12:38:56 +0700

    .env :)

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -8,6 +8,7 @@ class User extends CI_Controller
 		parent::__construct();
 		$this->load->helper('url');
 		$this->load->library(array('form_validation', 'session'));
+		$this->load->database();
 		$this->load->model('User_model');
 	}
 
