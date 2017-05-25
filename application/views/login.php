697dea6be56a4d27796c8cdc7c5425e7daec7d27
korobuckle <sangpa0@gmail.com>
2017-05-25 18:34:43 +0700

    ngerapihin view login

--- application/views/login.php
+++ application/views/login.php
@@ -2,16 +2,16 @@
 		<div class="col-sm-4"></div>
 		<!-- TODO: Center vertical -->
 		<div class="col-sm-4">
-			<div class="well well-lg" style="vertical-align: middle;">
-				<h1 style="padding-bottom: 20px;">Login</h1>
+			<div class="well well-lg" style="vertical-align: middle">
+				<h1 style="padding-bottom: 20px;">LOGIN</h1>
 				<form action="POST" class="form-horizontal">
 					<div class="form-group">
 						<input type="text" class="form-control" id="npm" placeholder="NPM">
-					</div><br>
+					</div>
 					<div class="form-group">
 						<input type="password" class="form-control" id="password" placeholder="Password">
 					</div>
-					<input type="submit" value="Submit" class="btn btn-primary">
+					<input type="submit" value="Submit" class="btn btn-warning">
 				</form>
 			</div>
 		</div>
