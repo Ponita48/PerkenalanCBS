18d11b50567b2870e1bf75353ee3e05a41fc0f2a
Dwiki Nurkurniawan <nurkurniawandwiki@gmail.com>
2017-07-23 16:02:59 +0700

    masukin ke form, hehe

--- application/views/request_angkatan.php
+++ application/views/request_angkatan.php
@@ -25,7 +25,7 @@
 </div>
 <div class="col-md-9">
 	<div class="well well-lg" style="vertical-align: middle">
-	<form action="<?php echo base_url(); ?>request_peserta/<?php echo $id_user; ?>" method="POST"></form>
+		<form action="<?php echo base_url(); ?>request_peserta/<?php echo $id_user; ?>" method="POST">
 			<div class="form-group">
 				<label for="nama">Nama</label>
 				<input type="nama" class="form-control" name="nama" placeholder="Nama teman anda">
@@ -34,10 +34,9 @@
 				<label for="tempat_lahir">Tempat Lahir</label>
 				<input type="text" class="form-control" name="tempat_lahir" placeholder="Tempat lahir teman anda">
 			</div>
-			<!-- TODO: jadiin select date -->
 			<div class="form-group">
 				<label for="tgl_lahir">Tanggal Lahir</label>
-				<input type="text" class="form-control" name="tgl_lahir" placeholder="Tanggal Lahir">
+				<input type="text" class="form-control" name="tgl_lahir" id="tgl_lahir" placeholder="Tanggal Lahir">
 			</div>
 			<div class="form-group">
 				<label for="alamat_kos">Alamat</label>
