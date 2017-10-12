9cad4befa45859619066a91f1de6037e042dcbb3
atepagung <agungteja64@gmail.com>
2017-10-12 17:52:23 +0700

    fix nominasi

--- application/views/nominasi.php
+++ application/views/nominasi.php
@@ -1,4 +1,4 @@
-<div class="wrapper-for-all">
+<div class="wrapper-for-all" id="nominasi">
 	<div class="embed-responsive embed-responsive-16by9">
 		<iframe class="embed-responsive-item" src="https://docs.google.com/forms/d/e/1FAIpQLSeRkPVRxU1cDXQBb1Kd_i_P3NmP8m8ryqm-a5OjHIgkUQ_Pjg/viewform?embedded=true">Loading...</iframe>
 	</div>
